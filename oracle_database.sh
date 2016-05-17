308f23e7b19a9988a53a9e1c23c11379f7b9c4b0
Gustavo Lopes <mail@geleia.net>
2016-05-17 15:11:25 +0200

    Trust DST Root CA X3

--- oracle_database.sh
+++ oracle_database.sh
@@ -1,5 +1,7 @@
 : ${ORACLE_MACHINE:=oracle-launcher.thehyve.net}
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 function get_oracle_db {
   if [[ -z "$ORACLE_SECRET" ]]; then
     echo "\$ORACLE_SECRET is not defined" >&2
@@ -9,7 +11,7 @@ function get_oracle_db {
   echo "Asking for oracle database" >&2
   if [[ ! -p fifo ]]; then mkfifo fifo; fi
   if [[ ! -p fifo2 ]]; then mkfifo fifo2; fi
-  ncat --ssl-verify --ssl -v \
+  ncat --ssl-trustfile "$DIR/dst_root_ca_x3.pem" --ssl-verify --ssl -v \
     $ORACLE_MACHINE 55397 < <(echo $ORACLE_SECRET ; cat fifo) > fifo2 &
   read result < fifo2
 
